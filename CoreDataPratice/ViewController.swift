2c002c60894a71c141cdacedfde5b538382d043c
yyeonjju <llilyha123@gmail.com>
2023-12-08 17:41:58 +0900

    코어데이터에 저장하고 불러오는 거 테스트 완료

--- CoreDataPratice/ViewController.swift
+++ CoreDataPratice/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 struct Person{
     var name : String
@@ -49,8 +50,41 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         setupTableView()
-
         setupConstraints()
+        
+        saveAtcoreData()
+    }
+    
+    func saveAtcoreData() {
+        //core data에 저장하고 싶은 데이터..!!
+        let heidi = Person(name: "heidi", email: "heidi@ggg.com")
+        
+        //NSPersistentContainer으로부터 NSManagedObjectContext를 가져옴
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext //NSPersistentContainer 타입에 viewContext라는 프로퍼티가 있어서 가능
+        
+        
+        //Entity를 가져옴 - 내가 원하는 데이터들을 어떤 Entity에 attribute로 저장하고 싶은데!
+        let entity = NSEntityDescription.entity(forEntityName: "Profile", in: context)
+        
+        //NSManagedObject를 만든다 -
+        if let entity = entity {
+            let profile = NSManagedObject(entity: entity, insertInto: context)
+            
+            //NSManagedObject에 값을 세팅
+            //앞서 지정해줬던 entity에 그 안에 생성해준 attribute이름을 "키"로 해서 저장해준다
+            profile.setValue(heidi.name, forKey: "name")
+            profile.setValue(heidi.email, forKey: "email")
+            
+            
+            do {
+              try context.save()  //NSManagedObjectContext에 있는 save 메서드로 core data에 저장해준다
+            } catch {
+              print(error.localizedDescription)
+            }
+
+        }
+        
     }
     
     func setupConstraints () {
@@ -93,6 +127,11 @@ class ViewController: UIViewController {
         self.listArray.append(listArray.count)
         self.tableView.reloadData() //항목추가하고 업데이트된 데이터로 테이블뷰 리로드
         print(listArray)
+        
+        
+        //코어데이터에 저장한거 불러와보기
+        fetchProfile()
+        
     }
     
     @objc func delBtnTapped() {
@@ -100,6 +139,20 @@ class ViewController: UIViewController {
         listArray.removeLast()
         self.tableView.reloadData() //항목추가하고 업데이트된 데이터로 테이블뷰 리로드
         print(listArray)
+    }
+    
+    func fetchProfile() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+            
+        do {
+           let contact = try context.fetch(Profile.fetchRequest()) as! [Profile]
+           contact.forEach {
+              print($0.name)
+          }
+        } catch {
+           print(error.localizedDescription)
+        }
     }
     
     
